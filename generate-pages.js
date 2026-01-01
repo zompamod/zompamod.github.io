1525dd4e97016dddac3b06c29b127b2e2e605347
zompamod <oursnagh@gmail.com>
2026-01-01 01:32:46 +0100

    Update generate-pages.js

--- generate-pages.js
+++ generate-pages.js
@@ -1,1 +1,21 @@
+const fs = require("fs");
+const path = require("path");
 
+const games = require("./games.js");
+
+const template = fs.readFileSync("game.html", "utf8");
+
+games.forEach(game => {
+  const dir = path.join(__dirname, game.id.toLowerCase());
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+
+  fs.writeFileSync(
+    path.join(dir, "index.html"),
+    template
+  );
+
+  console.log("Created:", game.id);
+});
