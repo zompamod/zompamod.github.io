292e20fa2b9c77b89e686701f056f502a42fff93
zompamod <oursnagh@gmail.com>
2026-01-27 16:53:26 +0100

    Update image URL for Demon Slayer game

--- games.js
+++ games.js
@@ -288,7 +288,7 @@ const GAMES = [
   {
     id:"DemonSlayerOnline",
     title:"Demon Slayer: Blade Battle",
-    image:"https://i.imgur.com/Gh8ucXw.jpeg",
+    image:"https://i.imgur.com/xfvED1U.jpeg",
     version:"v1.8.2",
     stars:"★★★★★",
     rating:"★★★★★ 4.8 (256,734 reviews)",
